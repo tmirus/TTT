be08b4d664d4d0af5cb25a0d320567b1e39622bf
tmirus <timmirus@aol.com>
2020-06-18 09:21:49 +0200

    improve visualization output and runtime

--- R/visualize_genes.R
+++ R/visualize_genes.R
@@ -3,51 +3,47 @@ visualize_genes <- function(counts, ids, img, clustering, gene.info, filepath = 
     spatial.cluster.legend <- spatial_plot(rownames(counts), ids, clustering, NULL, "discrete", nx, ny, ox, oy)
     spatial.cluster <- spatial_plot(rownames(counts), ids, clustering, img, "discrete", nx, ny, ox, oy)
 
-    print(str(gene.info[[2]]))
     all.genes <- unique(unlist(sapply(gene.info[[2]], function(x){names(x)}), use.names = F))
-    print(all.genes)
-    print(str(all.genes))
     heatmap.mat <- matrix(0, nrow = length(all.genes), ncol = length(unique(clustering)))
     rownames(heatmap.mat) <- all.genes
     colnames(heatmap.mat) <- unique(clustering)
 
-    print("reduced heatmap df")
     heatmap.counts <- apply(counts, 2, function(x){(x-mean(x)) / sd(x)})
-    heatmap.df <- c()
-    heatmap.full.df <- c()
-
-    for(cl in colnames(heatmap.mat)){
-        heatmap.df <- rbind(heatmap.df,
-                            cbind(cl, all.genes, log2(colMeans(counts[which(clustering == as.numeric(cl)), all.genes, drop = F])+1)))
+    counts <- heatmap.counts
+    
+    heatmap.df <- matrix("", ncol = 3, nrow = ncol(heatmap.mat)*length(all.genes))
+    for(i in 1:ncol(heatmap.mat)){
+        cl <- colnames(heatmap.mat)[i]
+        heatmap.df[((i-1)*length(all.genes)+1):(i*length(all.genes)),] <- cbind(cl, all.genes, colMeans(counts[which(clustering == as.numeric(cl)), all.genes, drop = F]))
     }
 
-    print("full heatmap df")
-    print(Sys.time())
-    print(dim(heatmap.counts))
     heatmap.counts <- heatmap.counts[order(clustering),]
-    for(g in all.genes){
+    heatmap.full.df <- matrix("", nrow = length(all.genes)*nrow(heatmap.counts), ncol = 3)
+    for(i in 1:length(all.genes)){
+        g <- all.genes[i]
          for(j in 1:nrow(heatmap.counts)){
-             heatmap.full.df <- rbind(heatmap.full.df,
-                                 c(j, g, heatmap.counts[j, g]))
+             heatmap.full.df[(i-1)*nrow(heatmap.counts)+j,] <- c(as.character(j), g, as.character(heatmap.counts[j, g]))
          }
     }
-    print(Sys.time())
+
+    gene.cluster <- hclust(dist(t(heatmap.counts[,all.genes]), "euclidean"), "complete")
+    gene.order <- gene.cluster$order
+    genes <- all.genes[gene.order]
     
-    print("prepare reduced heatmap df")
     heatmap.df <- as.data.frame(heatmap.df)
     colnames(heatmap.df) <- c("cluster", "gene", "expression")
     heatmap.df$expression <- as.numeric(as.character(heatmap.df$expression))
+    heatmap.df$cluster <- as.numeric(as.character(heatmap.df$cluster))
 
-    print("prepare full heatmap df")
     heatmap.full.df <- as.data.frame(heatmap.full.df)
     colnames(heatmap.full.df) <- c("spot", "gene", "expression")
     heatmap.full.df$expression <- as.numeric(as.character(heatmap.full.df$expression))
+    heatmap.full.df$spot <- as.numeric(as.character(heatmap.full.df$spot))
 
-    print("plot heatmaps")
     heatmap.plt <- ggplot(heatmap.df, aes(x=cluster, y=gene, fill = expression)) + geom_tile() + theme(axis.text.y = element_blank()) + xlab("cluster") + ylab("gene") +
-    scale_fill_gradient(low = "blue", high = "yellow")
+    scale_fill_gradient(low = "blue", high = "yellow") + scale_y_discrete(limits = genes)
     heatmap.full.plt <- ggplot(heatmap.full.df, aes(x=spot, y=gene, fill = expression)) + geom_tile() + theme(axis.text.y = element_blank()) + xlab("spot") + ylab("gene") +
-    scale_fill_gradient(low = "blue", high = "yellow")
+    scale_fill_gradient(low = "blue", high = "yellow") + scale_y_discrete(limits = genes)
 
     if(!is.null(filepath) && !dir.exists(filepath)){
         dir.create(filepath, recursive = TRUE)
@@ -62,7 +58,7 @@ visualize_genes <- function(counts, ids, img, clustering, gene.info, filepath = 
         dev.off()
     }
 
-    cluster.plots <- list(spatial = spatial.cluster, spatial.no_img = spatial.cluster.legend, heatmap = heatmap.plt)
+    cluster.plots <- list(spatial = spatial.cluster, spatial.no_img = spatial.cluster.legend, heatmap = heatmap.plt, heatmap.full = heatmap.full.plt)
 
     # if(!is.null(filepath)){
     #     pdf(paste(filepath, "spatial_genes.pdf", sep = "/"), width = 10, height = 10)
