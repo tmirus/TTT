df4842c8632553d38b60255542dae684c4742e69
tmirus <timmirus@aol.com>
2020-07-10 11:11:53 +0200

    add possibility to choose between HGNC symbols and ENSEMBL IDS for enrichment testing

--- R/test_enrichment.R
+++ R/test_enrichment.R
@@ -1,22 +1,44 @@
+#' perform enrichment testing based on differential gene expression analysis
+#' 
+#' @param analysis.data output of analyze_clustering
+#' @param clustering numeric vector assigning clusters to all spots (same order as spots in counts matrix)
+#' @param counts numeric non-negativ matrix containing gene expression for all spots (spots x genes)
+#' @param split_up_down boolean, should enrichment testing be performed separately on the list of up and downregulated genes for each cluster? default TRUE
+#' @param db_dataset ensembl dataset to use. default "mmusculus_gene_ensembl" (mouse), for human data use "hsapiens_gene_ensembl"
+#' @param exclusive boolean, should each gene only be considered for enrichment analysis in the cluster where it has the lowest p-value? default FALSE
+#' @param gene_id character denoting the gene symbols to use; default "hgnc" for HGNC symbols; use "ensembl" for ensembl gene ids
+#' @return list containing enrichmnet information for each cluster. if split_up_down is TRUE, 
+#' the list entry for each cluster is a list with two elements:\cr
+#' 1) up - table with enrichment information for upregulated genes (as returned by topGO)\cr
+#' 2) down - table with enrichment information for downregulated genes (as returned by topGO)\cr
+#' If split_up_down is false, the list contains one enrichment table for each cluster.
 #' @export
-test_enrichment <- function(analysis.data, clustering, counts, split_up_down = TRUE, db_dataset = 'mmusculus_gene_ensembl', exclusive = F){
+
+test_enrichment <- function(analysis.data, clustering, counts, split_up_down = TRUE, db_dataset = 'mmusculus_gene_ensembl', exclusive = F, gene_id = "hgnc"){
     deg.table <- analysis.data$differential_genes
     gene.table <- analysis.data$gene.cluster.table
 
+    if(gene_id == "hgnc"){
+        gene_id <- "external_gene_name"
+    }else if(gene_id == "ensembl"){
+        gene_id <- "ensembl_gene_id"
+    }
+
     # get GO database for testing
-    db <- useMart('ENSEMBL_MART_ENSEMBL',dataset=db_dataset, host="www.ensembl.org", verbose = TRUE)
-    go_ids <- getBM(
-        attributes=c('go_id', 'external_gene_name', 'namespace_1003'), 
-        filters = 'external_gene_name', 
-        values = colnames(counts), 
-        mart = db, 
-        verbose = F
-    )
+    suppressMessages({
+        db <- useMart('ENSEMBL_MART_ENSEMBL',dataset=db_dataset, host="www.ensembl.org", verbose = TRUE)
+        go_ids <- getBM(
+            attributes=c('go_id', gene_id, 'namespace_1003'), 
+            filters = gene_id, 
+            values = colnames(counts), 
+            mart = db, 
+            verbose = F
+        )
+    })
 
     # calculate enrichments for up and downregulated genes in each cluster
     enrichments <- list()
     for(cl in unique(deg.table$cluster)){
-        print(cl)
         # attribute genes only to the cluster with the lowest p-value
         if(exclusive){
             sub.table <- deg.table[which(deg.table$cluster == cl),]
